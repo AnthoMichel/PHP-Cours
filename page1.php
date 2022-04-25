1c47a254e972becf7f7a8647aa3ec5d66214bf89
Amar2050 <amarboutayeb@gmail.com>
2022-04-25 11:49:50 +0200

    Page1.php Le PHP interprète le html ! Cool

--- page1.php
+++ page1.php
@@ -1,3 +1,14 @@
-Page1 test !
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page One</title>
+</head>
+<body>
 
-<h1>Salut Page 1</h1>
+    <h1>Hello World !</h1>
+    
+</body>
+</html>
