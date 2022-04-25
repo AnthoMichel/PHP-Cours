f33e9b76b2890ed54dcdf1674ab26204f7878f4b
Amar2050 <amarboutayeb@gmail.com>
2022-04-25 15:41:21 +0200

    Opérateurs arithmétique // Test divers en PHP

--- test.php
+++ test.php
@@ -1,2 +1,50 @@
 <?php
+// Opérateurs arithmétique
+// $bug =0;
+// $i=1;
+// $a = 2;
+// echo "<br>";
+// echo $bug;
+// echo "<br>";
+// echo $i + $a ; // 3  
+// echo  $a - $i ; // 1
+// echo $i; // 1
+// echo "<br>";
+// echo $a; // 2 malgrè le calcul...  
+// echo "<br>"; // ne pas oublier les sauts de lignes
+// $b = 5;
+// echo $b * $a; // 10 
+// $c = $b * $a; // c
 
+// echo $z=0;
+// echo "<br>";
+// $z++; 
+// echo $z;// 1
+// echo "<br>";
+// echo $z--; // 0
+// echo "<br>";
+// echo $z+=4; // 4
+// echo "<br>";
+// echo $z= $z -4;;
+
+echo "<br>";
+
+$nb = 5; 
+
+$nb+=10; //15
+
+// $nb = $nb + 10 ;
+
+$nb+=15; // 30
+
+// echo $nb;
+
+$text= "Ceci est un texte ";
+
+$mot = "gros mot";
+
+$text ." ". $mot;
+
+$text.=$mot;
+
+echo $text;
