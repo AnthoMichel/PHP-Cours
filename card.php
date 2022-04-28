4075eaf5f580e347dd74c6b03c180cb62c38acf4
Amar2050 <amarboutayeb@gmail.com>
2022-04-28 16:54:27 +0200

    LOOP - Syntaxe Alternative + Require + CARD

---
+++ card.php
@@ -0,0 +1,4 @@
+<div class="card my-2 shadow p-4 text-center ">
+    <h2> <?= $game ?> </h2>
+    <button class="btn btn-success">Jouer</button>
+</div>
--- test.php
+++ test.php
@@ -18,10 +18,7 @@
 
     <?php foreach ($games as $game ): ?>
 
-        <div class="card my-2 shadow p-4 text-center ">
-            <h2> <?= $game ?> </h2>
-            <button class="btn btn-success">Jouer</button>
-        </div>
+        <?php require "card.php" ?>
 
     <?php endforeach; ?>
 
