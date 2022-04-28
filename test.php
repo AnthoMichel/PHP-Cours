ef846518339a99989843c587d539f6c24a3b7a3c
Amar2050 <amarboutayeb@gmail.com>
2022-04-28 09:37:42 +0200

    Suite Loop

--- test.php
+++ test.php
@@ -1,3 +1,43 @@
 <?php
 
 // BOUCLES 
+
+
+// $debut = 0;
+// while ($debut < 5) {
+//     echo "<p>Cool ! </p>";
+//     $debut++;
+// }
+
+// $i = 0;
+// // $i est une convention == itération
+// while($i <= 9){
+//     echo "<p> Cool ! $i </p>";
+//     $i++;
+// }
+
+// $j = 5;
+// // $i est une convention == itération
+// while($j > 0){
+//     echo " Cool ! $j ";
+//     $j--;
+// }
+
+// BOUCLE WHILE et TABLEAU
+
+
+$users = ["Jean","Marie","Mohamed","John"];
+
+// echo "$users[0] $users[1] $users[2] $users[3] ";
+
+// $a= 0;
+// while ($a < count($users) ) {
+//     echo "$users[$a] <br>";
+//     $a++;
+// }
+
+// BOUCLE FOR
+
+for ($i=0; $i < 10 ; $i++) { 
+    echo "Youpi $i  <br>";
+}
