fd9fd0a0b09ffcb33c994af2f570887ad3a58dfe
Amar2050 <amarboutayeb@gmail.com>
2022-05-06 14:25:41 +0200

    Lien GET + Contrôle + Redirection

--- objet.php
+++ objet.php
@@ -3,9 +3,18 @@
 
 // var_dump($_GET);
 
-if (empty($_GET["name"])) {
+if (!empty($_GET)) {
 
-    header("Location: list.item.php");
+    echo "Produit: " . $_GET["name"] . "<br>";
+
+    if($_GET['price'] > 100){
+        echo "Prix: " . $_GET["price"]  . " &euro;<br>";
+        echo "<p>Paiement en plusieurs fois possible ! </p>";
+    }else{
+        echo "Prix: " . $_GET["price"]  . " &euro;<br>";
+    }
+
 }else{
-    echo $_GET["name"];
+    
+    header("Location: list.item.php");
 }
